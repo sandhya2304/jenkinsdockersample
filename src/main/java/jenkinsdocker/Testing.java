b72a85cc6778075cee9666b86f54513d6435f017
sandhya2304 <sandhyasharma0423@gmail.com>
2020-10-07 18:04:45 +0530

    docker check

--- src/main/java/jenkinsdocker/Testing.java
+++ src/main/java/jenkinsdocker/Testing.java
@@ -4,7 +4,7 @@ public class Testing {
 
 	public static void main(String[] args) {
 		
-		System.out.println("git jenkins docker.... :) ");
+		System.out.println("git jenkins docker....Hello again :) ");
 
 	}
 
